eee5e5193d70ec778f97c6640692b3d6289a6497
DanMeloso <dg.meloso@outlook.com>
2019-06-18 13:44:37 -0300

    Feito até ex035

--- Exercicios/ex035.py
+++ Exercicios/ex035.py
@@ -1,3 +1,10 @@
 #Exercício Python 035: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se
 #elas podem ou não formar um triângulo.
 print('='*5, 'EXERCÍCIO 35', '='*5)
+r1 = float(input('Primeiro Segmento: '))
+r2 = float(input('Segundo Segmento: '))
+r3 = float(input('Terceiro Segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print(f'Os segmentos acima PODEM formar um triângulo')
+else:
+    print('Os segmentos acima NÃO PODEM formar um triângulo')
